e64b5c72765277b5f4fa8160ce2f6dca3a96b0ce
Yashwanth Aarem <aaremyashwanth@gmail.com>
2026-03-16 10:21:39 +0530

    little correction

--- count/r53.tf
+++ count/r53.tf
@@ -9,7 +9,7 @@ resource "aws_route53_record" "www" {
 resource "aws_route53_record" "aws"{
   count=length(var.domain)
   zone_id=var.zone_id
-  name="${var.instances[count.index].${var.domain}}"
+  name="${var.instances[count.index]}.${var.domain}"
   type="A"
   ttl=1
   records=[aws_instance.app_server[index(var.instances,"frontend")].public_ip]
